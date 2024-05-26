a5c43186ca15496268022211440ea788f386a8db
echooferen <edygithub@outlook.com>
2024-05-26 11:21:39 -0400

    fix: replace high quality img

--- src/components/ui/reward-modal.tsx
+++ src/components/ui/reward-modal.tsx
@@ -26,14 +26,14 @@ const RewardModal = ({ closeModal }: { closeModal: any }) => {
       <div className="drop-shadow-[0_0_2px_#F8EF00]">
         <div ref={modalRef} className="bg-[#201F07]">
           <div className="max-w-[600px] shadow-whiteyellow drop-shadow-xl max-md:max-w-[400px] max-sm:max-w-[300px]">
-            <h4 className="relative pb-5 pt-3 text-center text-3xl font-bold uppercase leading-9 tracking-[0.08em] text-whiteyellow after:absolute after:bottom-0 after:left-0 after:h-2 after:w-full after:bg-[url('/dividers/rewards-page-right-top-divider.svg')] after:content-[''] max-2xl:text-2xl max-lg:text-xl">
+            <h4 className="relative pb-5 pt-3 text-center text-3xl font-bold uppercase leading-9 tracking-[0.08em] text-whiteyellow after:absolute after:bottom-0 after:left-0 after:h-2 after:w-full after:bg-[url('/dividers/rewards-page-right-top-divider.svg')] after:content-[''] max-2xl:text-2xl max-lg:text-lg max-sm:pb-2 max-sm:text-xl">
               Invite your Exit Liquidity
             </h4>
-            <div className="rounded-md bg-[#151910] p-3 backdrop-blur-[2px]">
-              <h4 className="relative mt-8 pb-5 text-left text-2xl font-bold capitalize leading-9 tracking-[0.08em] text-whiteyellow max-2xl:text-xl max-lg:text-lg">
+            <div className="rounded-md bg-[#151910] p-3 backdrop-blur-[2px] max-sm:py-0">
+              <h4 className="relative mt-8 pb-5 text-left text-2xl font-bold capitalize leading-9 tracking-[0.08em] text-whiteyellow max-2xl:text-xl max-lg:text-lg max-sm:mt-0 max-sm:pb-2 max-sm:pt-1 max-sm:text-center">
                 We Make Farming your friends fun
               </h4>
-              <h4>
+              <h4 className="text-center max-sm:text-xs">
                 You get points when your invites earn points and when their
                 invites earn points. You get +5% bonus points when your invites
                 earn points. and +1% when their invites earn points. This is
@@ -45,7 +45,7 @@ const RewardModal = ({ closeModal }: { closeModal: any }) => {
                 alt=""
                 width={350}
                 height={500}
-                className="m-auto mt-4 max-sm:w-[250px]"
+                className="m-auto mt-4 max-sm:mt-1 max-sm:h-[200px] max-sm:w-[250px]"
               />
             </div>
           </div>
