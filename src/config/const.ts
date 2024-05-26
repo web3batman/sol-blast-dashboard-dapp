94ab3f2ad70665f952712b3991f76b907fc3b07a
echooferen <edygithub@outlook.com>
2024-05-26 11:20:10 -0400

    fix: replace new backend url

--- src/config/const.ts
+++ src/config/const.ts
@@ -1,3 +1,3 @@
-export const API_URL = 'https://l2-backend-d5e52f091795.herokuapp.com';
+export const API_URL = 'https://l2-layer-backend-b10973910e8e.herokuapp.com';
 
 export const VerifyMsg = `The quick brown fox jumps over the lazy dog`;
